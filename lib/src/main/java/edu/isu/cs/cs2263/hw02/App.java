ed1f7ff9744a9a3c3b09cc16170823f1d8481feb
codemaxx1 <garrnic3@isu.edu>
2021-02-14 22:47:03 -0700

    homework 2

---
+++ lib/src/main/java/edu/isu/cs/cs2263/hw02/App.java
@@ -0,0 +1,13 @@
+package edu.isu.cs.cs2263.hw02;
+
+import java.util.StringTokenizer;
+
+public class App {
+    public static void main(String[] args){
+        String s = "helloasdk;fghf;relksjdflk";
+        PushbackTokenizer<String> i = new Library<>();
+        i.nextToken();
+
+
+    }
+}
---
+++ lib/src/main/java/edu/isu/cs/cs2263/hw02/PushbackTokenizer.java
@@ -0,0 +1,12 @@
+package edu.isu.cs.cs2263.hw02;
+
+public interface PushbackTokenizer<E> {
+    // return the next token
+    String nextToken();
+
+    // returns true if and only if there are more tokens
+    boolean hasMoreTokens();
+
+    // the token read is pushed back, so it can be read again using nextToken.
+    void pushback();
+}
